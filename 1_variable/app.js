1d92514a8f3c6af79cb35d7f19cace72187c7935
Morgane Goulard <morganegoulard.56@gmail.com>
2022-07-29 17:07:35 +0200

    début js + exo 1 et 2

---
+++ 1_variable/app.js
@@ -0,0 +1,34 @@
+/*var nombre1=45;
+var nombre2=55;
+var somme=nombre1+nombre2;
+
+console.log(somme); // remplace le sout, à voir sur console du site web (inspecter)
+console.log(`${nombre1} + ${nombre2} = ${somme} `);*/
+
+// Identitié sans prompt
+/*var nom="Delon";
+var prenom="Alain";
+var age=32;
+console.log(`Votre nom est ${nom} ${prenom} et vous avez ${age} ans`)*/
+
+// Identité avec prompt 
+/*var nom=prompt("Votre nom : ");
+var prenom=prompt("Votre prénom : ");
+var age=prompt("Votre age : ");
+// prompt remplace le Scanner system in 
+alert(`Votre nom est ${nom} ${prenom} et vous avez ${age} ans`);*/
+
+//exo1
+var nombre1=parseInt(prompt("Choisissez un nombre : "));
+var nombre2=parseInt(prompt("Choisissez un nombre : "));
+var somme=nombre1+nombre2;
+
+alert(`Bienvenue sur la page de formulaire`);
+console.log(`nombre 1 : ${nombre1} / nombre 2 : ${nombre2} / somme : ${somme}`)
+alert(`le résultat du calcul est : ${somme}`);
+
+//exo2
+/*var d=prompt("Distance parcourue en m: ");
+var t=prompt("Temps écoulé en sec : ");
+alert(`Votre vitesse est : ${d/t} m/s`);
+console.log(`Votre vitesse est : ${d/t} m/s`);*/
