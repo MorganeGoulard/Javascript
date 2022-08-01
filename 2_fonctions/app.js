77ec29989e4415a2e941b7735e6afc7c75e2cb30
Morgane Goulard <morganegoulard.56@gmail.com>
2022-08-01 12:05:03 +0200

    function, anonymous function expression & arrow function

---
+++ 2_fonctions/app.js
@@ -0,0 +1,45 @@
+// créer une fonction qui rtourne une somme de 2 nombres
+function calculSomme(nombre1, nombre2){ //pas besoin du type
+    if(typeof nombre1 !== "number" || typeof number2 !== "number"){
+        throw new Error("Veuillez entrer des nombres valides");
+    }
+    return nombre1+nombre2;
+}; 
+// pour typer les variables en js, on fera avec typescript 
+
+//slugify("macbook pro", "+") ==> "macbook+pro"
+//slugify("macbook pro", "*") ==> "macbook*pro"
+//slugify("macbook pro", "-") ==> "macbook-pro"
+function slugify(str, delimiter){
+    const strToLowerCase = str.toLowerCase();
+    const strToArray = strToLowerCase.split(" ");
+    return strToArray.join(delimiter);
+};
+
+// # Anonymous function expression
+const slugifyV2 = function (str, delimiter){
+    const strToLowerCase = str.toLowerCase();
+    const strToArray = strToLowerCase.split(" ");
+    return strToArray.join(delimiter);
+};
+// slugifyv2 contient la référence de la fonction 
+// en JS, les fonctions ce sont des objets !!!
+// permet d'optimiser des algorithmes 
+
+// # Arrow function 
+const slugifyV3=(str, delimiter)=>{
+    const strToLowerCase = str.toLowerCase();
+    const strToArray = strToLowerCase.split(" ");
+    return strToArray.join(delimiter);
+};
+
+// créer une fonction qui retourne l'inverse d'une chaine
+const reverseStr=function(str){
+    return str.split("").reverse().join("");
+};
+
+const reverseStrV2 = (str) => {
+    return str.split("").reverse().join("");
+};
+
+const reverseStrV3 = (str) => str.split("").reverse().join("");
